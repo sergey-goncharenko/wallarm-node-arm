be5a5edecb3d4a16c764cd5cf76134674f230137
Sergey <38881254+sergey-goncharenko@users.noreply.github.com>
2020-12-29 14:58:53 +0300

    new vm template version

---
+++ install-docker-ubuntu.sh
@@ -0,0 +1,36 @@
+﻿#!/bin/bash
+
+###	This file should be run in SUDO mode
+
+### The script file needs to be executable, i.e.
+#	chmod +x script.sh
+
+#	Update package index
+
+apt-get update
+
+#	Install tools
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+#	Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+#	Setup stable repo
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+#	Update package index (again)
+apt-get update
+
+#	Install latest version of Docker CE
+apt-get install docker-ce -y
+
+#	Run Hello World
+docker run -d -e DEPLOY_USER='$1' -e DEPLOY_PASSWORD='$2' -e NGINX_BACKEND='$3' -e TARANTOOL_MEMORY_GB=$4 -p 80:$5 $6
+#docker run hello-world
